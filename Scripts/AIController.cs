3edb9fbb0172f6a531269487a3977c72feec36ba
firismanda <ryuka.1111@gmail.com>
2020-12-21 11:26:34 +0700

    init

---
+++ Scripts/AIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    public Transform ball;
+
+    [Range(0, 1)]
+    public float skill;
+
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = Mathf.Lerp(transform.position.x, ball.position.x, skill);
+        transform.position = newPos;
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera playerCam;
+
+
+    void Update()
+    {
+        if(Input.touchCount != 1)
+            return;
+
+        var ray = playerCam.ScreenPointToRay(Input.touches[0].position);
+        var hitInfo = new RaycastHit();
+        if(Physics.Raycast(ray, out hitInfo)){
+            if(hitInfo.transform.name != "Bounds South")
+                return;
+
+            var x = hitInfo.point.x;
+            var newPos = transform.position;
+            newPos.x = x;
+            transform.position = newPos;
+        }    
+    }
+}
